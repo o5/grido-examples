764c38bb8120963946ed189f909b88058fdeb997
Petr Bugyík <petr@bugyik.cz>
2012-12-24 14:33:19 +0100

    Added macro "scache", Typos

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -1,16 +1,20 @@
 <?php
 
-use Nette\Application\UI\Presenter;
-
-abstract class BasePresenter extends Presenter
+abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
     public function createTemplate($class = NULL)
     {
         $template = parent::createTemplate($class);
-        $template->registerHelper('strtoupper', 'strtoupper');
 
         $baseUri = $this->context->parameters['baseUri'];
         $template->baseUri = $baseUri ? $baseUri : $template->basePath;;
+
+        $latte = new Nette\Latte\Engine;
+        $macros = Nette\Latte\Macros\MacroSet::install($latte->compiler);
+        $macros->addMacro('scache', '?>?<?php echo strtotime(date(\'Y-m-d hh \')); ?>"<?php');
+
+        $template->registerFilter($latte);
+        $template->registerHelper('strtoupper', 'strtoupper');
 
         return $template;
     }
