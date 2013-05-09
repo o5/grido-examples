df3b285b6853610b8476c380a493f0f180b31352
Petr Bugyík <petr@bugyik.cz>
2013-05-09 19:04:29 +0200

    Removed phpdocs

--- app/controls/Grido/Boolean.php
+++ app/controls/Grido/Boolean.php
@@ -1,21 +1,10 @@
 <?php
 
-/**
- * This file is part of the Grido (http://grido.bugyik.cz)
- *
- * Copyright (c) 2011 Petr Bugyík (http://petr.bugyik.cz)
- *
- * For the full copyright and license information, please view
- * the file license.md that was distributed with this source code.
- */
-
 namespace Grido\Components\Columns;
 
 /**
  * Boolean column.
  *
- * @package     Grido
- * @subpackage  Components\Columns
  * @author      Pavel Kryštůfek (http://www.krystufkovi.cz)
  */
 class Boolean extends Text
