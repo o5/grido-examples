9421b3320eb7f3f5935ad60bae7aa349bddbf274
Petr Bugyík <petr@bugyik.cz>
2013-07-26 16:30:55 +0200

    Renamed application.js => main.js; application.ajax.js => main.ajax.js

---
+++ www/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    //uncomment below if you want to change the datepicker...
+    //$.fn.grido.Grid.prototype.initDatepicker = function() {
+    //    this.$element.on('focus', 'input.date').mySuperTruperDatePicker();
+    //};
+
+    $('.grido').grido({
+        ajax: false
+    });
+});
